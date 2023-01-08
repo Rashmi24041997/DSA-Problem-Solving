d7a0aecf6a9d316dc66bf8c3e639385ba8b340a8
Rashmi Gupta <rashmimittal2497@gmail.com>
2023-01-08 17:38:08 +0530

    7th Jan problem solution

--- DSA Problem Solving/Data Structures/ListNode.cs
+++ DSA Problem Solving/Data Structures/ListNode.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DSA_Problem_Solving.Medium.LinkedList
+namespace DSA_Problem_Solving
 {
     public class ListNode
     {
--- DSA Problem Solving/Easy/LinkedList/Linked List Mid.cs
+++ DSA Problem Solving/Easy/LinkedList/Linked List Mid.cs
@@ -1,11 +1,10 @@
-﻿using DSA_Problem_Solving.Medium.LinkedList;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DSA_Problem_Solving.Easy
+namespace DSA_Problem_Solving.Easy.LinkedList
 {
     internal class Linked_List_Mid
     {
@@ -15,16 +14,16 @@ namespace DSA_Problem_Solving.Easy
             ListNode mid = head.next;
             ListNode crnt = head.next;
             while (crnt != null)
-            { 
+            {
                 if (crnt.next != null)
                 {
-                    mid= mid.next;
+                    mid = mid.next;
                     crnt = crnt.next.next;
                 }
                 else
                     break;
             }
-                    return mid;
+            return mid;
         }
 
     }
