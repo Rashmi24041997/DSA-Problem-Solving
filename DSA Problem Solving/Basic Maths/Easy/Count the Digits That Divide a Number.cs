ffa5ac9e0e813eb3e8667e94314c9724d8ca7575
Rashmi <rashmimittal2497@gmail.com>
2025-02-15 00:40:01 +0530

    Added cmnts n solved easy string problems

--- DSA Problem Solving/Basic Maths/Easy/Count the Digits That Divide a Number.cs
+++ DSA Problem Solving/Basic Maths/Easy/Count the Digits That Divide a Number.cs
@@ -10,17 +10,18 @@ namespace DSA_Problem_Solving.Basic_Maths.Easy
     {
         /*
          2520. Count the Digits That Divide a Number
-Given an integer num, return the number of digits in num that divide num.
-
-An integer val divides nums if nums % val == 0.
-
- 
-
-Example 1:
-
-Input: num = 7
-Output: 1
-Explanation: 7 divides itself, hence the answer is 1.*/
+            Given an integer num, return the number of digits in num that divide num.
+            
+            An integer val divides nums if nums % val == 0.
+            
+             
+            
+            Example 1:
+            
+            Input: num = 7
+            Output: 1
+            Explanation: 7 divides itself, hence the answer is 1.
+        */
         public static int CountDigits(int num)
         {
             if (num > -4 && num < 4) return num;
--- DSA Problem Solving/Data Structures/Stack.cs
+++ DSA Problem Solving/Data Structures/Stack.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,7 @@ namespace DSA_Problem_Solving.Data_Structures
 
         public Stack()
         {
-            Arr = new int[100];
+            Arr = new int[10000];
             top = -1;
         }
 
@@ -51,6 +53,12 @@ namespace DSA_Problem_Solving.Data_Structures
     {
         public class Easy
         {
+            /*
+             Problem: Given a list of integers, find the previous smaller element for each element in the list.
+             If there is no smaller element, return -1 for that position.
+             Time Complexity: O(n^2)
+             Space Complexity: O(n)
+             */
             public static List<int> PrevSmaller(List<int> A)
             {
                 int n = A.Count;
@@ -60,7 +68,7 @@ namespace DSA_Problem_Solving.Data_Structures
                 {
                     int low = -1;
                     int num = A[i];
-                    for (int j = i - 1; j >=0; j--)
+                    for (int j = i - 1; j >= 0; j--)
                     {
                         if (num > A[j])
                         {
@@ -72,6 +80,13 @@ namespace DSA_Problem_Solving.Data_Structures
                 }
                 return ans;
             }
+
+            /*
+             Problem: Given two arrays nums1 and nums2, find the next greater element for each element in nums1 in nums2.
+             If there is no greater element, return -1 for that position.
+             Time Complexity: O(n * m)
+             Space Complexity: O(n)
+             */
             public static int[] NextGreaterElement(int[] nums1, int[] nums2)
             {
                 int[] ans = new int[5];
@@ -99,6 +114,12 @@ namespace DSA_Problem_Solving.Data_Structures
                 return result;
             }
 
+            /*
+             Problem: Given two arrays nums1 and nums2, find the next greater element for each element in nums1 in nums2 using brute force.
+             If there is no greater element, return -1 for that position.
+             Time Complexity: O(n * m)
+             Space Complexity: O(n)
+             */
             public static int[] NextGreaterElementBF(int[] nums1, int[] nums2)
             {
                 int[] result = new int[nums1.Length];
@@ -132,7 +153,12 @@ namespace DSA_Problem_Solving.Data_Structures
                 return result;
             }
 
-            public static void SortStack(DSA_Problem_Solving.Data_Structures.Stack<int> stack)
+            /*
+             Problem: Given a stack, sort the elements in ascending order.
+             Time Complexity: O(n log n)
+             Space Complexity: O(n)
+             */
+            public static void SortStack(Stack<int> stack)
             {
                 int[] arr = new int[stack.Size()];
                 int i = 0;
@@ -148,6 +174,123 @@ namespace DSA_Problem_Solving.Data_Structures
                 }
             }
 
+            /*
+             Problem: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+             An input string is valid if:
+             - Open brackets must be closed by the same type of brackets.
+             - Open brackets must be closed in the correct order.
+             - Every close bracket has a corresponding open bracket of the same type.
+             Time Complexity: O(n)
+             Space Complexity: O(n)
+             */
+            public static bool IsValidParenthesis(string s)
+            {
+                int n = s.Length;
+                if (n % 2 != 0) return false;
+
+                char f = s[0], l = s[n - 1];
+                if (f == ')' || f == '}' || f == ']' || l == '(' || l == '{' || l == '[') return false;
+
+                System.Collections.Generic.Stack<char> stack = new();
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    char p;
+                    char c = s[i];
+                    switch (c)
+                    {
+                        case '(':
+                            if (stack.Count < 1 || stack.Pop() != ')')
+                                return false;
+                            break;
+                        case ')':
+                            stack.Push(c);
+                            break;
+                        case '{':
+                            if (stack.Count < 1 || stack.Pop() != '}')
+                                return false;
+                            break;
+                        case '}':
+                            stack.Push(c);
+                            break;
+                        case '[':
+                            if (stack.Count < 1 || stack.Pop() != ']')
+                                return false;
+                            break;
+                        case ']':
+                            stack.Push(c);
+                            break;
+                    }
+                }
+                return stack.Count < 1;
+            }
+        }
+
+        public class Medium
+        {
+            public class MinStack
+            {
+                private int capacity, min, top;
+                private int[] Arr;
+                public int Count => top + 1;
+                public bool IsEmpty => top == -1;
+
+                public MinStack()
+                {
+                    capacity = 30000;
+                    Arr = new int[30000];
+                    top = -1;
+                    min = int.MinValue;
+                }
+
+                /*
+                 Problem: Implement a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+                 Time Complexity: O(1) for each operation
+                 Space Complexity: O(n)
+                 */
+                public void Push(int val)
+                {
+                    top++;
+                    if (top > Arr.Length - 1)
+                    {
+                        Array.Resize(ref Arr, capacity * 2);
+                        capacity *= 2;
+                    }
+                    Arr[top] = val;
+                    min = Math.Min(min, val);
+                }
+
+                public void Pop()
+                {
+                    if (top == -1)
+                        throw new InvalidOperationException("Stack is empty.");
+
+                    Arr[top] = -1;
+                    top--;
+                    min = Math.Min(min, Arr[top]);
+                }
+
+                public int Top()
+                {
+                    if (top == -1)
+                        throw new InvalidOperationException("Stack is empty.");
+
+                    return Arr[top];
+                }
+
+                public int GetMin()
+                {
+                    return min;
+                }
+            }
+
+            /**
+             * Your MinStack object will be instantiated and called as such:
+             * MinStack obj = new MinStack();
+             * obj.Push(val);
+             * obj.Pop();
+             * int param_3 = obj.Top();
+             * int param_4 = obj.GetMin();
+             */
         }
     }
 }
